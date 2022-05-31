0ad63e28f0b227a34f9fc466a2ee20d18af8d602
daahunii <wjdekgns35@naver.com>
2022-06-01 02:28:25 +0900

    upload file

---
+++ app.js
@@ -0,0 +1,30 @@
+// test
+// const player = {
+//     name : "dahun",
+//     points : 10,
+//     fat : true
+// }
+
+// console.log(player);
+// player.lastname = "Jeong";
+// console.log(player);
+
+// function plus(first, second){
+//     console.log(first + second);
+// }
+
+// plus(3, 5);
+
+// const player2 = {
+//     name : "dahun",
+//     points : 10,
+//     fat : true,
+//     sayhello : function(name){
+//         console.log(name + " hello");
+//     }
+// }
+
+// player2.sayhello("dahun");
+
+const age = prompt("How old are you?");
+console.log(age);
